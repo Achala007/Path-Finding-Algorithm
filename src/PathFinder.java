f34b6352bdeb5c4f4daee5535cc5c571417d7ac9
Achala007 <achalayasas007@gmail.com>
2017-04-17 23:39:05 +0530

    set path

--- src/PathFinder.java
+++ src/PathFinder.java
@@ -42,8 +42,7 @@ public class PathFinder {
         //ArrayList<Node> path2 = new PathFinder().distance(randomMatrix, Ai, Aj, Bi, Bj, Euclidean(), "Euclidean", false);
         ArrayList<Node> path3 = new PathFinder().distance(randomMatrix, Ai, Aj, Bi, Bj, Chebyshev(), "Chebyshev",false);
 
-       show(randomMatrix, true, Ai, Aj, Bi, Bj, path3);
-
+       show(randomMatrix, true, Ai, Aj, Bi, B
 
     }
 
