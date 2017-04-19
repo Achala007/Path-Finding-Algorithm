f6cd394be014d87862c55bb535425f8fd1757325
Achala007 <achalayasas007@gmail.com>
2017-04-19 23:42:44 +0530

    end and start

--- src/PathFinder.java
+++ src/PathFinder.java
@@ -79,7 +79,12 @@ public class PathFinder {
     }
 
     /**
-     * @p
+     * @param matrix The boolean matrix from the framework given
+     * @param starti     start x value
+     * @param startj     start y value
+     * @param endi     end x value
+     * @param endj     end x value
+     * @return The path nodes
      */
     ArrayList<Node> distance(boolean[][] matrix, int starti, int startj, int endi, int endj, double dDistance, String name, boolean isManhat) {
 
