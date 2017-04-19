aec4c4b2c470d909bba46aca14afbbe4fff75ef4
Achala007 <achalayasas007@gmail.com>
2017-04-19 23:42:09 +0530

    euclidian

--- src/PathFinder.java
+++ src/PathFinder.java
@@ -66,7 +66,7 @@ public class PathFinder {
 
     //Euclidean values.
     //Here the diagonal distance is 1.4
-    public static double Euclidean() {
+    public static doubn() {
         double dDistance = 1.4;
         return dDistance;
     }
