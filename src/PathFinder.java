3fd3cb0291ebefb624743c394829ede775ac5927
Achala007 <achalayasas007@gmail.com>
2017-04-21 23:45:25 +0530

    chck nodes2

--- src/PathFinder.java
+++ src/PathFinder.java
@@ -149,7 +149,14 @@ public class PathFinder {
 
 
             //Checking Top Node
-            
+            if (current.x - 1 >= 0) {
+                testNode = grid[current.x - 1][current.y];
+                double newDistance = current.distance + hAndVDistance;
+                if (!testNode.blocked && !testNode.visited && testNode.distance > newDistance) {
+                    testNode.distance = newDistance;
+                    testNode.parent = current;
+                    PriorityQueue.add(testNode);
+                }
             }
 
 
